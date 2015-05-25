cdc6b98002d0a9cee4b2ab840736aa6a17b43be1
Charles Lo <jerries.lo@gmail.com>
2015-05-26 00:49:43 +0800

    1. page2 finish

--- leyu/src/com/leyu/PageHome.java
+++ leyu/src/com/leyu/PageHome.java
@@ -1,19 +1,25 @@
 package com.leyu;
 
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class PageHome extends Fragment {
 	static final String TAG = PageHome.class.getSimpleName();
+	private String[] list = { "河童", "殭屍", "吸血鬼", "妖精", "天狗" };
+	private ArrayAdapter<String> listAdapter;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -30,6 +36,14 @@ public class PageHome extends Fragment {
 
 			}
 		});
+
+		TextView listTitle = (TextView) rootView.findViewById(R.id.list_title);
+		listTitle.setText("奇妙的大自然");
+		ListView lv = (ListView) rootView.findViewById(R.id.listView);
+		// lv.addHeaderView(textView, null , false);
+		listAdapter = new ArrayAdapter<String>(getActivity(),
+				R.layout.listitem_01, R.id.listTextView, list);
+		lv.setAdapter(listAdapter);
 		return rootView;
 	}
 }
